c7d49586cfbc97cfa5e20fc86a0472b82da9390f
thineisan <>
2019-11-21 11:38:02 +0900

    project code 1st commit

---
+++ resources/views/shop.blade.php
@@ -0,0 +1,46 @@
+@extends('master')
+
+@section('content')
+
+    <div class="container">
+
+        @if (session()->has('success_message'))
+            <div class="alert alert-success">
+                {{ session()->get('success_message') }}
+            </div>
+        @endif
+
+        @if (session()->has('error_message'))
+            <div class="alert alert-danger">
+                {{ session()->get('error_message') }}
+            </div>
+        @endif
+
+        <div class="jumbotron text-center clearfix">
+            <h2>Shopping Cart Application</h2>
+            <p>An example Laravel App that demos the basic functionality of a typical e-commerce shopping cart.</p>
+            <p>
+                <a href="https://github.com/thinei/Shopping-Cart-Laravel" class="btn btn-success btn-lg" target="_blank">GitHub Repo</a>
+            </p>
+        </div> <!-- end jumbotron -->
+
+        @foreach ($products->chunk(4) as $items)
+            <div class="row">
+                @foreach ($items as $product)
+                    <div class="col-md-3">
+                        <div class="thumbnail">
+                            <div class="caption text-center">
+                                <a href="{{ url('shop', [$product->slug]) }}"><img src="{{ asset('img/' . $product->image) }}" alt="product" class="img-responsive"></a>
+                                <a href="{{ url('shop', [$product->slug]) }}"><h3>{{ $product->name }}</h3>
+                                <p>{{ $product->price }}</p>
+                                </a>
+                            </div> <!-- end caption -->
+                        </div> <!-- end thumbnail -->
+                    </div> <!-- end col-md-3 -->
+                @endforeach
+            </div> <!-- end row -->
+        @endforeach
+
+    </div> <!-- end container -->
+
+@endsection
